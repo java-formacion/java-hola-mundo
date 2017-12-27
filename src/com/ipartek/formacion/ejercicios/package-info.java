235d20652c624a0c0debf07a5c26d72f5d32a0f7
anderuraga <ander.uraga@gmail.com>
2017-12-27 09:33:25 +0100

    planteado ejercicios basicos

---
+++ src/com/ipartek/formacion/ejercicios/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Ejercicios basicos de Java para repasar
+ *
+ * @see http://puntocomnoesunlenguaje.blogspot.com.es/p/ejercicios.html
+ * @author ur00
+ *
+ */
+package com.ipartek.formacion.ejercicios;
---
+++ src/com/ipartek/formacion/ejercicios/principiantes/Ejercicio1.java
@@ -0,0 +1,22 @@
+package com.ipartek.formacion.ejercicios.principiantes;
+
+/**
+ * Escribe un programa Java que realice lo siguiente: declarar una variable N de
+ * tipo int, una variable A de tipo double y una variable C de tipo char y
+ * asigna a cada una un valor. A continuación muestra por pantalla: El valor de
+ * cada variable. La suma de N + A La diferencia de A - N El valor numérico
+ * correspondiente al carácter que contiene la variable C. Si por ejemplo le
+ * hemos dado a N el valor 5, a A el valor 4.56 y a C el valor ‘a’, se debe
+ * mostrar por pantalla:
+ * 
+ * @author ur00
+ *
+ */
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
