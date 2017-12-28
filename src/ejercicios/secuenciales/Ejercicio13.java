5a0c791dba5e587e47e496b2618b5b47704d08d6
java <java@PORT-Z14.ipartekaula.com>
2017-12-28 11:27:04 +0100

    Otro ejercicio más

--- src/ejercicios/secuenciales/Ejercicio13.java
+++ src/ejercicios/secuenciales/Ejercicio13.java
@@ -1,9 +1,25 @@
 package ejercicios.secuenciales;
+import java.util.Scanner;
+/**
+ * Programa que pida por teclado la fecha de nacimiento de una persona (dia, mes, año) y calcule su número de la suerte.
+ * @author java
+ *
+ */
 
 public class Ejercicio13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int dia, mes, ano, cifraTotal, primeraCifra, segundaCifra, terceraCifra, cuartaCifra;
+		System.out.println("Cual es tu fecha de nacimiento?");
+		System.out.println("Escribe el día");
+		dia = sc.nextInt();
+		System.out.println("Escribe el mes");
+		mes= sc.nextInt();
+		System.out.println("Escribe el año");
+		ano=sc.nextInt();
+		cifraTotal = dia+mes+ano;
+		
 
 	}
 
