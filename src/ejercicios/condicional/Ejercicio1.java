c39ffb928cc0df1562075fd55e6f083fb3def1a2
java <java@PORT-Z14.ipartekaula.com>
2017-12-28 11:52:19 +0100

    unos poco más

---
+++ src/ejercicios/condicional/Ejercicio1.java
@@ -0,0 +1,16 @@
+package ejercicios.condicional;
+import java.util.*;
+/**
+ * Programa Java que lea un número entero por teclado y calcule si es par o impar.
+ * @author java
+ *
+ */
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- src/ejercicios/secuenciales/Ejercicio13.java
+++ src/ejercicios/secuenciales/Ejercicio13.java
@@ -1,7 +1,11 @@
 package ejercicios.secuenciales;
+
 import java.util.Scanner;
+
 /**
- * Programa que pida por teclado la fecha de nacimiento de una persona (dia, mes, año) y calcule su número de la suerte.
+ * Programa que pida por teclado la fecha de nacimiento de una persona (dia,
+ * mes, año) y calcule su número de la suerte.
+ * 
  * @author java
  *
  */
@@ -9,17 +13,22 @@ import java.util.Scanner;
 public class Ejercicio13 {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int dia, mes, ano, cifraTotal, primeraCifra, segundaCifra, terceraCifra, cuartaCifra;
+		Scanner sc = new Scanner(System.in);
+		int dia, mes, ano, cifraTotal, primeraCifra, segundaCifra, terceraCifra, cuartaCifra, numeroSuerte;
 		System.out.println("Cual es tu fecha de nacimiento?");
 		System.out.println("Escribe el día");
 		dia = sc.nextInt();
 		System.out.println("Escribe el mes");
-		mes= sc.nextInt();
+		mes = sc.nextInt();
 		System.out.println("Escribe el año");
-		ano=sc.nextInt();
-		cifraTotal = dia+mes+ano;
-		
+		ano = sc.nextInt();
+		cifraTotal = dia + mes + ano;
+		primeraCifra = cifraTotal / 1000;		
+		segundaCifra = (cifraTotal/100)%10;		
+		terceraCifra=(cifraTotal/10)%10;		
+		cuartaCifra= cifraTotal%1000;
+		numeroSuerte=primeraCifra+segundaCifra+terceraCifra+cuartaCifra;
+		System.out.println("Tu número de la suerte es el "+numeroSuerte);
 
 	}
 
