47326ede4a9524a0382c8d60e4729aabc05161a0
java <java@LAPTOP-C4HVDJKR.ipartekaula.com>
2017-12-28 09:53:58 +0100

    Prueba

--- src/com/ipartek/formacion/helloworld/capitulo2/Alumnos.java
+++ src/com/ipartek/formacion/helloworld/capitulo2/Alumnos.java
@@ -1,90 +1,91 @@
 package com.ipartek.formacion.helloworld.capitulo2;
 
 public class Alumnos {
-
-	static String[] aAlumnos = { "Eder", "Janai", "Ander", "Edu", "Borja", "Mikel Larru", "Judit", "", "Adrian", "Paul",
-			"Pavel", "Ivan", "", "Mikel Parra", "Jagoba", "Eritz" };
-
+	static String [] alumnos = {"Eder","Janair","Ander","Edu",
+								"Borja","Mikel","Judith","  -",
+								"Adrian","Paul","Pavel","Ivan",
+								"  -", "Mikel P", "Jagoba","Eritz"
+	};
+	public static void main(String[] args) {
+		
+		System.out.println("- Numero alumnos es: "+ numeroAlumnos());
+		//Listar();
+		System.out.println("************************************************");
+		System.out.println("Vision alumno:\n");
+		listarVisionAlumno();
+		System.out.println("\nVision Profesor: \n");
+		listarVisionProfesor();
+		System.out.printf("\n\nLe toca leer a: " + selecLector());
+		
+	}
 	/**
-	 * Muestra por consola todos los Alumn@s con su poscion. ej:
-	 *
-	 * 0 - Pepe <br>
-	 * 1 - Manolo <br>
-	 * .... <br>
-	 * etc <br>
+	 * Selecciona alumno random
+	 * @return
 	 */
-	static void listar() {
-
-		for (int i = 0; i < aAlumnos.length; i++) {
-			System.out.println(aAlumnos[i]);
+	private static String selecLector() {
+		int r;
+		do {
+			r = (int)(Math.random()*16);			
 		}
-
-	}
-
-	static void listarVisionAlumno() {
-
-		for (int i = 0; i < aAlumnos.length; i++) {
-
-			System.out.print(aAlumnos[i] + ",    ");
-
-			// saltar linea cada 4 alumnos
-			if ((i + 1) % 4 == 0) {
-				System.out.println("");
-			}
-
-		}
-
-	}
-
-	static void listarVisionProfesor() {
-
-		for (int i = (aAlumnos.length - 1); i >= 0; i--) {
-
-			System.out.print(aAlumnos[i] + ",    ");
-
-			// saltar linea cada 4 alumnos
-			if (i % 4 == 0) {
-				System.out.println("");
-			}
-
-		}
-
+		while(alumnos[r].equals("  -")); 
+		
+		return alumnos[r];
 	}
 
 	/**
-	 * Muestra el nombre del alumn@ que se presenta volunatario para comenzar a leer
-	 * teoria
-	 *
-	 * @return String nombre del alumn@
+	 * Muestra por consola todos los alumnos/as
 	 */
-	static String voluntarioLeer() {
-		int numAleatorio = (int) (Math.random() * aAlumnos.length);
-		return aAlumnos[numAleatorio];
+	static void Listar() {
+		for (int i = 0; i < alumnos.length; i++) {
+			System.out.println(i + " - " + alumnos[i]);
+		}
 	}
-
+	
 	/**
-	 * indica el numero de alumnos en clase
-	 *
-	 * @return int con el numero de alumn@s
+	 * Devuelve int tamaño array de alumnos/as.
+	 * @return
 	 */
 	static int numeroAlumnos() {
-		return aAlumnos.length;
+		return alumnos.length;
+		
 	}
-
-	public static void main(String[] args) {
-
-		// System.out.println("volunatio es: " + voluntarioLeer());
-
-		// listarVisionProfesor();
-
-		listarVisionAlumno();
-
-		System.out.println("Limpiar consola");
-
-		/*
-		 * System.out.println("Numero de Alumnos = " + numeroAlumnos());
-		 * System.out.println("-------------------------------------------"); listar();
-		 */
+	static void listarVisionProfesor() {
+		int i;
+		for ( i = alumnos.length-1; i >=0; i--) {
+			System.out.printf("   "+alumnos[i] + " ");
+			calcSeparacion( i,1);
+		
+		}
 	}
-
+	static void listarVisionAlumno() {
+		for (int i = 0; i < alumnos.length; i++) {
+			System.out.printf("   "+alumnos[i] + " ");
+			calcSeparacion( i,0);
+			
+			
+		}
+	}
+	/**
+	 * Ordenar de 4 en cuatro y que todos los nombres ocupen lo mismo
+	 * @param i
+	 * @param p
+	 */
+	static void calcSeparacion(int i, int p) {
+		//Que haya la misma distancia entre palabras
+		int difNombre = 8-alumnos[i].length();
+		for (int j = 1; j <= difNombre; j++) {
+			if(j<difNombre) {					
+			System.out.print(" ");
+			}else if (j==difNombre){
+				System.out.printf("|");
+			}
+		}
+		//Salto de linea para alumnos
+		if((i+1)%4==0 && p==0) {
+			System.out.println();
+		//Salto de linea para profesores
+		}else if((i)%4==0 && p==1) {
+			System.out.println();
+		}
+	}
 }
