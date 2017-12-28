73206366bfba9ccc70acd32048f4044dd987d808
java <java@Formacion-TOS14.ipartekaula.com>
2017-12-28 11:27:13 +0100

    Ejercicio 1 echo

--- src/com/ipartek/formacion/ejercicios/secuencial/Ejercicio1.java
+++ src/com/ipartek/formacion/ejercicios/secuencial/Ejercicio1.java
@@ -1,5 +1,7 @@
 package com.ipartek.formacion.ejercicios.secuencial;
 
+import java.util.Scanner;
+
 /**
  * 
  * Programa Java que lea dos números enteros por teclado y los muestre por
@@ -10,9 +12,13 @@ package com.ipartek.formacion.ejercicios.secuencial;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		
-		
-		
+
+		Scanner sc = new Scanner(System.in);
+		int numero1 = sc.nextInt();
+		int numero2 = sc.nextInt();
+		System.out.println("El numero 1 es: " + numero1);
+		System.out.println("El numero 2 es: " + numero2);
+
 	}
 
 }
