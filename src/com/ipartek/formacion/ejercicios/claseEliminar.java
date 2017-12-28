6494a3ff0bcfde7882d0218957bad783061ec6fb
java <java@LAPTOP-O7PJ5MCC.ipartekaula.com>
2017-12-28 09:53:43 +0100

    pr cambios

---
+++ src/com/ipartek/formacion/ejercicios/claseEliminar.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion.ejercicios;
+
+public class claseEliminar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/com/ipartek/formacion/ejercicios/package-info.java
+++ src/com/ipartek/formacion/ejercicios/package-info.java
@@ -1,8 +1,9 @@
 /**
  * Ejercicios basicos de Java para repasar
- *
+ 
  * @see http://puntocomnoesunlenguaje.blogspot.com.es/p/ejercicios.html
  * @author ur00
  *
  */
 package com.ipartek.formacion.ejercicios;
+// vamos a comprobar
