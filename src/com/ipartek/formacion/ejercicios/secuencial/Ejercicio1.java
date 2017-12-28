f48dcfeae014e9554e643bc559b9c5ebb49e725b
java <java@Formacion-TOS14.ipartekaula.com>
2017-12-28 10:58:22 +0100

    Ejercicio para hacer

---
+++ src/com/ipartek/formacion/ejercicios/secuencial/Ejercicio1.java
@@ -0,0 +1,18 @@
+package com.ipartek.formacion.ejercicios.secuencial;
+
+/**
+ * 
+ * Programa Java que lea dos números enteros por teclado y los muestre por
+ * pantalla.
+ *
+ */
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
