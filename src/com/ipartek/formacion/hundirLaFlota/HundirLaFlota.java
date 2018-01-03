7a67b560cba593f94bca36a8ccbabaf6dd9ad4d0
java <java@PORT-Z6.ipartekaula.com>
2018-01-03 11:34:16 +0100

    Mejorado hundir la flota

--- src/com/ipartek/formacion/hundirLaFlota/HundirLaFlota.java
+++ src/com/ipartek/formacion/hundirLaFlota/HundirLaFlota.java
@@ -1,17 +1,19 @@
 package com.ipartek.formacion.hundirLaFlota;
 
+import java.util.Scanner;
+
 public class HundirLaFlota {
 
 	static String[][] tablero = new String[10][10];
 	static int intentos = 0;
 	static int aciertos = 0;
-	static int fallos = 0;
 
 	public static void main(String[] args) {
 
 		rellenarTablero();
-		//rellenarCoordenadas();
-		//solicitarCoordenadas();
+		rellenarCoordenadas();
+		mostrarTablero();
+		solicitarCoordenadas();
 
 	}
 
@@ -23,26 +25,40 @@ public class HundirLaFlota {
 		for (int i = 0; i < tablero.length; i++) {
 			for (int j = 0; j < tablero.length; j++) {
 				tablero[i][j] = "A";
-				System.out.print(tablero[i][j] + " ");
 			}
-			System.out.println();
 		}
 
 	}
 
 	/*
-	 * Añado al tablero los barcos Tres barcos tamaño 1 Dos barcos tamaño 2 Un barco
-	 * tamaño 3
+	 * Muestra el estado actual del tablero
+	 */
+	private static void mostrarTablero() {
+
+		System.out.println("Tablero actual: ");
+		for (int i = 0; i < tablero.length; i++) {
+			for (int j = 0; j < tablero.length; j++) {
+				System.out.print(tablero[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+	}
+
+	/*
+	 * Añado al tablero los barcos: Tres barcos tamaño 1, Dos barcos tamaño 2, Un
+	 * barco tamaño 3
 	 */
 	private static void rellenarCoordenadas() {
 		// Añadir tres barcos tamaño 1
-		tablero[2][1] = "1";
+		tablero[1][2] = "1";
 		tablero[4][5] = "1";
 		tablero[7][7] = "1";
 
 		// Añadir dos barcos tamaño 2
-		tablero[2][1] = "2";
-		tablero[2][1] = "2";
+		tablero[5][2] = "2";
+		tablero[6][2] = "2";
 
 		tablero[1][7] = "2";
 		tablero[2][7] = "2";
@@ -55,8 +71,44 @@ public class HundirLaFlota {
 	}
 
 	private static void solicitarCoordenadas() {
-		// TODO Auto-generated method stub
 
+		while ((intentos < 20) && (aciertos < 9)) {
+			Scanner sc = new Scanner(System.in);
+
+			System.out.println("Dame una coordenada \"i\": ");
+			sc = new Scanner(System.in);
+			int coordenadaI = sc.nextInt();
+
+			System.out.println("Dame una coordenada \"j\": ");
+			sc = new Scanner(System.in);
+			int coordenadaJ = sc.nextInt();
+
+			comprobarCoordenadas(coordenadaI, coordenadaJ);
+		}
+
+		if (aciertos == 9) {
+			System.out.println("Enhorabuena! Has hundido la flota");
+		} else {
+			System.out.println("Oooh! Otra vez será");
+		}
+
+	}
+
+	private static void comprobarCoordenadas(int coordenadaI, int coordenadaJ) {
+
+		if ((tablero[coordenadaI][coordenadaJ] == "1") || (tablero[coordenadaI][coordenadaJ] == "2")
+				|| (tablero[coordenadaI][coordenadaJ] == "3")) {
+			aciertos++;
+			tablero[coordenadaI][coordenadaJ] = "0"; // 0 indica barco tocado
+			System.out.println("Tocado!!!!");
+		} else if (tablero[coordenadaI][coordenadaJ] == "A") {
+			intentos++;
+			System.out.println("Agua!!!!!");
+		} else {
+			intentos++;
+			System.out.println("Coordenada repetida!!!!!");
+		}
+		System.out.println();
 	}
 
 }
