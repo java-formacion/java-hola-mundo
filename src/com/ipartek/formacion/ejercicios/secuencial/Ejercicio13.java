05bd6cfbdacb9b7ca4cbfcf8de0aa94e74540713
yoshi <yoshi@DESKTOP-84N2D8A>
2018-01-07 20:55:24 +0100

    secuencialfin

--- src/com/ipartek/formacion/ejercicios/secuencial/Ejercicio13.java
+++ src/com/ipartek/formacion/ejercicios/secuencial/Ejercicio13.java
@@ -1,5 +1,7 @@
 package com.ipartek.formacion.ejercicios.secuencial;
 
+import java.util.Scanner;
+
 /**
  * 
  * Programa que pida por teclado la fecha de nacimiento de una persona (dia,
@@ -14,7 +16,25 @@ package com.ipartek.formacion.ejercicios.secuencial;
 public class Ejercicio13 {
 
 	public static void main(String[] args) {
-		
+
+		Scanner sc = new Scanner(System.in);
+		int dia, mes, ano, suma, cifra1, cifra2, cifra3, cifra4, suerte;
+		System.out.println("¡Calculemos tu numero de la suerte!");
+		System.out.println("Empecemos por el año en que naciste:");
+		ano = sc.nextInt();
+		System.out.println("Ahora continuemos con el mes (numero de mes) en el que naciste");
+		mes = sc.nextInt();
+		System.out.println("Y por ultimo el dia en el que naciste:");
+		dia = sc.nextInt();
+		suma = dia + mes + ano;
+		//System.out.println("Comprobacion");
+		//suma=sc.nextInt();
+		cifra1=suma/1000;
+		cifra2=suma/100%10;
+		cifra3=suma/10%10;
+		cifra4=suma%10;
+		suerte=cifra1+cifra2+cifra3+cifra4;
+		System.out.println("Nuestros expertos astrologos, han calculado que tu numero de la suerte es el... \n"+suerte);
 		
 
 	}
