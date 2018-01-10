28371ade399a0b3311824b31adf08d7a6a5425e1
java <java@PORT-Z9.mshome.net>
2018-01-10 10:57:13 +0100

    Mejoras

---
+++ src/com/ipartek/formacion/ejercicios/generales/Ejercicio_004.java
@@ -0,0 +1,12 @@
+package com.ipartek.formacion.ejercicios.generales;
+
+/**
+ * 
+ * Programa Java que lea un número entero N y 
+ * muestre la tabla de multiplicar de ese número.
+ *
+ */
+
+public class Ejercicio_004 {
+
+}
